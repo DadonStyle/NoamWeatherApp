8455c6eaea962b019911ff3e3a48b4c68779eac1
Noam <noamoni9@gmail.com>
2024-05-10 00:43:05 +0300

    add auto complete query

---
+++ src/queries/useAutoComplete.tsx
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import apis from "../api/weatherApi";
+
+const useAutoComplete = (searchString: string, enable?: boolean) => {
+  const {
+    data: searchOptions,
+    isError,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["autoComplete", searchString],
+    queryFn: async () => await apis.getAutoCompleteOptions(searchString),
+    staleTime: 1_000 * 60,
+    enabled: enable,
+  });
+
+  return {
+    searchOptions,
+    isError,
+    isLoading,
+    refetch,
+  };
+};
+
+export default useAutoComplete;
