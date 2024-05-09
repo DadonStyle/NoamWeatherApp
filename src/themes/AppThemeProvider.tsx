34d17e757e2e2775f574f076e3194ef6905204a3
Noam <noamoni9@gmail.com>
2024-05-09 16:53:00 +0300

    setup, add mui + theme and react query

---
+++ src/themes/AppThemeProvider.tsx
@@ -0,0 +1,41 @@
+import { ThemeProvider, createTheme, responsiveFontSizes } from "@mui/material";
+import { ReactNode } from "react";
+
+// enable custom button variant declarations
+declare module "@mui/material/Button" {
+  interface ButtonPropsVariantOverrides {
+    secondary: true;
+  }
+}
+
+export const AppThemeProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const theme = responsiveFontSizes(
+    createTheme({
+      // preferred to use the default mui platte
+      typography: {
+        fontFamily: "Inter, sans-serif",
+        h1: {},
+      },
+      components: {
+        MuiButton: {
+          variants: [
+            {
+              props: { variant: "secondary" },
+              style: {},
+            },
+          ],
+          styleOverrides: {
+            root: {
+              "&:hover": {},
+              "&:disabled": {},
+            },
+          },
+        },
+      },
+    })
+  );
+
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
