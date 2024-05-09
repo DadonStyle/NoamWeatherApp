e78ccf6275e5f322fa222737949498adeecfa222
Noam <noamoni9@gmail.com>
2024-05-10 00:39:34 +0300

    small change daily change query

--- src/queries/useDailyDataPerCity.tsx
+++ src/queries/useDailyDataPerCity.tsx
@@ -1,19 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import apis from "../api/weatherApi";
 
-const useFetchDailyDataPerCity = (cityKey: string) => {
+const useDailyDataPerCity = (cityKey: string) => {
   const { data, isError, isLoading, refetch } = useQuery({
-    queryKey: ["dailyData"],
+    queryKey: ["dailyData", cityKey],
     queryFn: async () => await apis.getDailyDataPerCity(cityKey),
     // refetchInterval: 300_000, // updates the data every interval
   });
 
   return {
-    data,
+    data: data?.[0],
     isError,
     isLoading,
     refetch,
   };
 };
 
-export default useFetchDailyDataPerCity;
+export default useDailyDataPerCity;
