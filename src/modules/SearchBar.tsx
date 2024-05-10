5920379ad36be2d0dc54bf11ec8986f5a9a82e98
Noam <noamoni9@gmail.com>
2024-05-10 16:03:31 +0300

    rename Search to SearchBar

---
+++ src/modules/SearchBar.tsx
@@ -0,0 +1,51 @@
+import { Dispatch, FC, SetStateAction, useState } from "react";
+import SearchComponent from "../components/SearchComponent/SearchComponent";
+import useDebounce from "../hooks/useDebounce";
+import useAutoComplete from "../queries/useAutoComplete";
+import { CityDetailsType } from "../types/types";
+
+const SearchBar: FC<{
+  cityName: string;
+  updateData: Dispatch<SetStateAction<CityDetailsType>>;
+}> = ({ cityName, updateData }) => {
+  const [searchString, setSearchString] = useState<string>(cityName || "");
+  const debouncedString = useDebounce(searchString, 500);
+  const isSearchEnabled = debouncedString?.length > 1;
+  const { searchOptions } = useAutoComplete(debouncedString, isSearchEnabled);
+
+  const handleOnChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchString(e.currentTarget.value);
+  };
+
+  const findCityInSearchOptions = (cityName: string) => {
+    if (!isSearchEnabled) return;
+    const match = searchOptions.find(
+      (item: any) => item.LocalizedName === cityName
+    );
+    return match;
+  };
+
+  const handleOnSelectOption = (_: unknown, values: string) => {
+    if (!isSearchEnabled || !values) return setSearchString("");
+    const cityData = findCityInSearchOptions(values);
+    if (!cityData) return;
+    const cityDetailsObj: CityDetailsType = {
+      key: cityData.Key,
+      name: cityData.LocalizedName,
+      country: cityData.Country.LocalizedName,
+    };
+    if (cityData?.LocalizedName) setSearchString(cityData.LocalizedName);
+    updateData(cityDetailsObj);
+  };
+
+  return (
+    <SearchComponent
+      onChangeFunc={handleOnChangeInput}
+      onSelectFunc={handleOnSelectOption}
+      searchString={debouncedString}
+      searchOptions={searchOptions}
+    />
+  );
+};
+
+export default SearchBar;
