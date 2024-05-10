c254f5c5a9e48b0a84662cbddde9fbc79c18ee26
Noam <noamoni9@gmail.com>
2024-05-11 02:40:48 +0300

    add const for app default value and small util change

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,4 +1,5 @@
 export const getDateFromEpoch = (epoch: number) => {
+  if (!epoch) return;
   const date = new Date(epoch * 1000);
   const dayNumeric = date.getDate();
   const month = date.toLocaleString("default", { month: "long" });
@@ -6,6 +7,7 @@ export const getDateFromEpoch = (epoch: number) => {
 };
 
 export const getFormattedDateText = (epoch: number) => {
+  if (!epoch) return;
   const date = new Date(epoch * 1000);
   const day = date.toLocaleDateString("en", {
     weekday: "long",
