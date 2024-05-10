fa2eaf9e5c91e25bdab23a8df3b9ceb101976143
Noam <noamoni9@gmail.com>
2024-05-11 02:39:24 +0300

    add InfoBox Component

--- src/components/InfoBox/InfoBox.tsx
+++ src/components/InfoBox/InfoBox.tsx
@@ -1,15 +1,24 @@
-import { FC, PropsWithChildren } from "react";
+import { FC, ReactNode } from "react";
 import { FlexBox } from "../StyledComponents/FlexBox/FlexBox";
+import styles from "./InfoBox.module.css";
+import { Theme } from "@mui/material";
 
-interface InfoBoxProps extends PropsWithChildren {
-  header: string;
+interface InfoBoxProps {
+  label: string;
+  midValue: ReactNode;
+  bottomValue?: ReactNode;
 }
 
-const InfoBox: FC<InfoBoxProps> = ({ header, children }) => {
+const InfoBox: FC<InfoBoxProps> = ({ label, midValue, bottomValue }) => {
   return (
-    <FlexBox>
-      {header}
-      {children}
+    <FlexBox
+      bgcolor={(theme: Theme) => theme.palette.common.purple}
+      color={(theme: Theme) => theme.palette.common.white}
+      className={styles.infoBoxContainer}
+    >
+      <FlexBox>{label}</FlexBox>
+      <FlexBox>{midValue}</FlexBox>
+      <FlexBox> {bottomValue}</FlexBox>
     </FlexBox>
   );
 };
